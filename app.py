e931b8681aefd1c7d4100abfcdd807bd8f2c5ef7
Dannie Chen <dannie.chen.bj@gmail.com>
2020-07-31 21:13:59 -0500

    modify app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", context = "Testing")
+    return render_template("home.html")
 
 @app.route("/european")
 def european():
